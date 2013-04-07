6643f54bfa750d26db2a65fbf1694cf429dfa55c
Josh Adams <josh@isotope11.com>
2013-04-07 12:57:06 -0500

    Got everyone's sounds working

--- src/com/example/adamsfaces/MainActivity.java
+++ src/com/example/adamsfaces/MainActivity.java
@@ -8,43 +8,62 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
-	
-	private Button mGracie;
+
+  private Button mGracie;
+  private Button mMatthew;
+  private Button mJosh;
+  private Button mKristen;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mGracie = (Button) findViewById(R.id.btnGracie);
+    mGracie.setOnClickListener(new SoundClickListener(R.raw.graciesound));
+
+    mMatthew = (Button) findViewById(R.id.btnMatthew);
+    mMatthew.setOnClickListener(new SoundClickListener(R.raw.matthewsound));
+
+    mJosh = (Button) findViewById(R.id.btnJosh);
+    mJosh.setOnClickListener(new SoundClickListener(R.raw.joshsound));
+
+    mKristen = (Button) findViewById(R.id.btnKristen);
+    mKristen.setOnClickListener(new SoundClickListener(R.raw.kristensound));
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
+  private class SoundClickListener implements OnClickListener {
+    int mSoundFileResource;
+
+    public SoundClickListener(int soundFileResource){
+      mSoundFileResource = soundFileResource;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        mGracie = (Button) findViewById(R.id.btnGracie);
-        mGracie.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				playGracieSound();
-			}
-		});
-    }
-    
-    private void playGracieSound() {
-    	MediaPlayer player = MediaPlayer.create(this, R.raw.graciesound);
-    	player.start();
-    	player.setOnCompletionListener(new OnCompletionListener() {
-			@Override
-			public void onCompletion(MediaPlayer mp) {
-				// Release player.
-				mp.release();
-			}
-		});
+    public void onClick(View view) {
+      playSound(view);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    private void playSound(View view) {
+      MediaPlayer player = MediaPlayer.create(view.getContext(), mSoundFileResource);
+      player.start();
+      player.setOnCompletionListener(new OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+          // Release player.
+          mp.release();
+        }
+      });
     }
+
+  }
 }
